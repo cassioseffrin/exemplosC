72a13aaf482c64d1da841d57eb97ac0a3a5836dd
Cassio Seffrin <cassioseffrin@gmail.com>
2018-12-01 17:30:26 -0200

    ultima colaboracao projeto

--- Jose.c
+++ Jose.c
@@ -1,61 +1,87 @@
 
 #include <stdio.h>
 
-int contarVivos (int arr[], int quant ){
-    int vivos=0;
-     for (int i = 0; i < quant; i++){
-       if ( arr[i] == 1){
-           vivos++;
-       }
+int inicarMatanca(int arr[], int pos, int nroPri)
+{
+    int vivos = contarVivos(arr, nroPri);
+    while (vivos != 1)
+    {
+        for (int i = pos + 1; i < nroPri; i++)
+        {
+            if (arr[i] == 1)
+            {
+                arr[i] = 0;
+                pos = passarFaca(arr, i + 1, nroPri);
+                break;
+            }
+            else if (i + 1 == nroPri && contarVivos(arr, nroPri) > 1)
+            {
+                pos = -1;
+            }
+        }
+        if (pos + 1 == nroPri && contarVivos(arr, nroPri) > 1)
+        {
+            pos = -1;
+        }
+        vivos = contarVivos(arr, nroPri);
+    }
+
+    for (int i = 0; i < nroPri; i++)
+    {
+        printf("\n array[%d]: %d", i, arr[i]);
+    }
+}
+
+int passarFaca(int arr[], int pos, int nroPri)
+{
+    for (int i = pos; i < nroPri; i++)
+    {
+        if (arr[i] == 1)
+        {
+            return i;
+            break;
+        }
+    }
+    if (contarVivos(arr, nroPri) > 1)
+    {
+        return passarFaca(arr, 0, nroPri);
+    }
+    else
+    {
+        return 99999;
+    }
+}
+
+int contarVivos(int arr[], int quant)
+{
+    int vivos = 0;
+    for (int i = 0; i < quant; i++)
+    {
+        if (arr[i] == 1)
+        {
+            vivos++;
+        }
     }
     return vivos;
 }
 
-int calcular(int quant)
-{
-    printf("Prisioneiros  : %d", quant);
-    int arr[quant];
-
-    int pos = 0;
-    int skip = 1;
-
-    for (int i = 0; i < quant; i++){
-        arr[i] = 1;
-        // printf("\nArray[%d]: %d", i, arr[i]);
-    }
-    // inicia o processo
-    for (int i = 0;  (pos+skip) < quant; i++) {
-        arr[pos+skip]=0; 
-        pos = pos+skip+1;
-    }
-
-    // fazer esta parte 
-    // int vivos = quant;
-    // while( vivos == 1){
-    //     for (int i = 0;  (pos+skip) < quant; i++) {
-    //         arr[pos+skip]=0; 
-    //         pos = pos+skip+1;
-    //     }
-    //     vivos = contarVivos(arr, quant);
-    // } 
-
-
-    printf("\nVivos: %d \n", contarVivos(arr, quant));
-
-    //imprime resultado
-   for (int i = 0; i < quant; i++){ 
-         printf("\nArray[%d]: %d", i, arr[i]);
-    }
-    return 0;
-}
 
 int main(void)
 {
     int matador;
     int morre;
-    int quant;
+    int quant = 10;
     int skip = 1;
     printf("Prisioneiros  :");
     scanf("%d", &quant);
-    printf("resultado: %d", calcular(quant));
+ 
+    int arr[quant];
+
+    int pos = 0;
+
+    for (int i = 0; i < quant; i++)
+    {
+        arr[i] = 1;
+    }
+    inicarMatanca(arr, pos, quant);
 }
